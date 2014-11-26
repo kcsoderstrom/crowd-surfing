b58bf8a1eccc9a258ecbd78f0a61b273579121d4
KC Soderstrom <jacquelinesoderstrom@gmail.com>
2014-11-26 17:38:59 -0500

    Undo add event id to photos table

--- db/migrate/20141126223331_add_event_id_to_photos.rb
+++
@@ -1,6 +0,0 @@
-class AddEventIdToPhotos < ActiveRecord::Migration
-  def change
-    add_column :photos, :event_id, :integer
-    add_index :photos, :event_id, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126223331) do
+ActiveRecord::Schema.define(version: 20141126174739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,10 +60,8 @@ ActiveRecord::Schema.define(version: 20141126223331) do
     t.datetime "pic_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "event_id"
   end
 
-  add_index "photos", ["event_id"], name: "index_photos_on_event_id", unique: true, using: :btree
   add_index "photos", ["profile_id"], name: "index_photos_on_profile_id", using: :btree
 
   create_table "profiles", force: true do |t|
